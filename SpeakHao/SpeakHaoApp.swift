d6f82d88e24612c21061df35d669546c4eb7bc72
nfahrisalim <nfahrisalim2@gmail.com>
2026-05-01 14:19:10 +0700

    Initial Commit

---
+++ SpeakHao/SpeakHaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakHaoApp.swift
+//  SpeakHao
+//
+//  Created by Muh. Naufal Fahri Salim on 5/1/26.
+//
+
+import SwiftUI
+
+@main
+struct SpeakHaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
